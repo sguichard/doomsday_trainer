5a25b98cb239fb4ef6f8a604ad45a42c01931b9e
Steven Guichard <ssguichard@gmail.com>
2012-02-06 19:36:49 -0800

    Allowed multiple trials

--- doomsday_trainer.py
+++ doomsday_trainer.py
@@ -1,15 +1,17 @@
 import datetime
 import random
+import sys
 
 days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
 def ordinal(beg,end):
+    #Return a random date between two given years (inclusive)
     b=datetime.date(beg,1,1).toordinal()
     e=datetime.date(end,12,31).toordinal()
     return datetime.date.fromordinal(random.randint(b,e))
 
 def random_date():
-#return a random date, with preference to nearby dates
+    #Return a random date, with preference to nearby dates
     rand = random.random()
     this_year = datetime.datetime.now().year
     if rand < 0.1:
@@ -53,14 +55,14 @@ def check(field,answer):
     if str(answer)==raw_input():
         print 'Correct, %s\n' % response
     else:
-        print 'Sorry, you suck, %s\n' % response
+        print 'Incorrect, the answer is %s\n' % response
 
 def weekday(date):
-#Takes a Date object and returns the weekday number, converting to Doomsday type
+    #Takes a Date object and returns the weekday number, converting to Doomsday type
     return (date.weekday()+1)%7
 
 def doomsday_walkthrough(date):
-#run if user guesses wrong date. Asks for date for each step
+    #Run if user guesses wrong date. Asks for date for each step
     year=date.year
     century_day=doomsday_century(year/100)
     fields=['Century Day',century_day, \
@@ -70,16 +72,31 @@ def doomsday_walkthrough(date):
     for i in range(0,8,2):
         check(fields[i],fields[i+1])
 
-def main():
+def run_test():
     date=random_date()
     print 'What day of the week is '+str(date)
     guess =raw_input()
     if guess==str(weekday(date)):
         print 'Correct! \n'
+        return True
     else:
-        print "\nWrong, let's start from the beginning"
+        print "\nIncorrect, let's start from the beginning"
         doomsday_walkthrough(date)
-print 'starting'
+        return False
+
+def main():
+    if len(sys.argv) > 1:
+        if not sys.argv[1].isdigit() or int(sys.argv[1]) < 1:
+            print 'Please enter a valid number of trials as the first (and only) argument'
+            return
+        trials = int(sys.argv[1])
+    else:
+        trials = 1
+    correct = 0
+    for i in range(trials):
+        correct += run_test()
+        print 'You have answered {0} out of {1} trial(s) correctly'.format(correct, trials)
+
 if __name__=='__main__':
     main()
 
